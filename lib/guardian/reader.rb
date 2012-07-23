f7c0ab79af38e61ad167d6aa2be163847061bee7
Rizwan Tejpar <rtejpar@gmail.com>
2012-07-23 14:21:22 -0400

    continued to build Guardian::Reader class -- includes project, template, root, guards parsing

--- lib/guardian/reader.rb
+++ lib/guardian/reader.rb
@@ -2,32 +2,43 @@ require 'yaml'
 
 class Guardian::Reader
 
-	attr_reader :data, :file, :supported
+	attr_reader :available, :file, :data, :project, :template, :root, :guards
 
-	def initialize(file = '', extensions = %w[.yaml .yaml.example])
+	def initialize(file = '')
 		@file = get_filename(file)
-		@supported = extensions
+		@available = get_available_config
 
-		load
+		load unless @available.empty?
 	end
 
 	private
 
 	def load
-		filename = File.join(Guardian::CONFIG_PATH, @file)
-		@data = YAML::load(File.open(filename)) if File.file?(filename)
+		path = File.join(Guardian::CONFIG_PATH, @file)
+		@data = YAML::load(File.open(path)) if @available.include?(@file)
 		@data = {} if @data.nil? || @data == false
+
+		parse
+	end
+
+	def get_available_config
+		files = Dir.entries(Guardian::CONFIG_PATH)
+		files.delete_if { | f | f.start_with?('.') || f == Guardian::CONFIGURATION }
+		files.delete_if { | f | !f.end_with?('.yaml', '.yaml.example') }
+		files
 	end
 
 	def get_filename(filename)
-		return filename if (filename.nil? || filename.empty?)
+		return '' if filename.nil?
 
-		filename << '.yaml' unless filename.end_with?(table_for(@supported))
+		filename << '.yaml' unless filename.end_with?('.yaml', '.yaml.example')
 		filename
 	end
 
-	def table_for(collection, *args)
-		"Got #{collection} and #{args.join(',')}"
+	def parse
+		@project = @data['project']
+		@guards = @data['guards']
+		@template = @data['template'] if %w[general].include?(@data['template'])
+		@root = @data['root'] if File.exist?(@data['root']) unless @data['root'].nil? || File.file?(@data['root'])
 	end
-
 end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -4,16 +4,103 @@ require_relative '../lib/guardian'
 describe Guardian::Reader do
 	include GuardianSpecHelper
 
-	before(:each) do
-		@reader = Guardian::Reader.new
+	def write_read_remove(settings = nil, file = 'test.yaml')
+		yaml = File.join(Guardian::CONFIG_PATH, file)
+
+		unless settings.nil?
+			write_settings(settings[:project],
+										 settings[:guards],
+										 settings[:template],
+										 settings[:root],
+										 settings[:single_guards],
+										 yaml)
+
+		else
+			write_to_yaml(yaml, {'test' => 'data'})
+		end
+
+		reader = Guardian::Reader.new(file)
+		FileUtils.rm_f([yaml])
+
+		return reader
 	end
 
-	it "should only support .yaml and .yaml.example files by default" do
-		@reader.supported.length.should == 2
-		@reader.supported.include?(%w[.yaml .yaml.example])
+
+	context "Fetch Available Configurations" do
+		it "should only allow .yaml and .yaml.example files from #{Guardian::CONFIG_PATH} to be loaded" do
+			yaml = File.join(Guardian::CONFIG_PATH, 'test.yaml')
+			example = File.join(Guardian::CONFIG_PATH, 'test.yaml.example')
+
+			FileUtils.touch([yaml, example])
+			list = Guardian::Reader.new.available
+			FileUtils.rm_f([yaml, example])
+
+			list.length.should == 2
+			(list - %w[test.yaml test.yaml.example]).length.should == 0
+		end
+
+		it "should not allow hidden files or settings.yaml to be loaded" do
+			reader = write_read_remove(nil, '.dotfile.yaml')
+			reader.available.length.should == 0
+			File.exists?(File.join(Guardian::CONFIG_PATH, 'settings.yaml')).should == true
+		end
+
+		it "should automatically add the .yaml extension to supported files if not specified" do
+			write_read_remove.file.should == 'test.yaml'
+		end
 	end
 
+	context "Config File Parsing" do
+		it "should return the project name if set" do
+			settings = {:project => {'project' => 'zero'}}
+			write_read_remove(settings).project.should == 'zero'
+		end
+
+		it "should return the project template type if supported" do
+			settings = {:template => {'template' => 'general'}}
+			reader = write_read_remove(settings)
+			reader.template.should == 'general'
+
+			settings[:template]['template'] = 'unsupported'
+			reader = write_read_remove(settings)
+			reader.template.should == nil
+		end
+
+		it "should return the project root if it is a valid directory" do
+			settings = {:root => {'root' => '/bin'}}
+			reader = write_read_remove(settings)
+			reader.root.should == '/bin'
+
+			settings[:root]['root'] = '/bin/sh'
+			reader = write_read_remove(settings)
+			reader.root.should == nil
+		end
+
+		it "should return the specified guards if set" do
+			settings = {:guards => {'guards' => %w[bundler rspec livereload]}}
+			reader = write_read_remove(settings)
+			reader.guards.length.should == 3
+			(reader.guards - %w[bundler livereload rspec]).length == 0
+		end
+
+		it "should return the patterns for each guard if set" do
 
 
+		end
+
+		it "should only return the pattern for each guard if a watch or watch and block is set" do
+
+		end
+
+
+		it "should return nil for any value not set" do
+			reader = write_read_remove({})
+			reader.project.nil?.should == true
+			reader.guards.nil?.should == true
+			reader.template.nil?.should == true
+			reader.root.nil?.should == true
+		end
+
+	end
 
 end
